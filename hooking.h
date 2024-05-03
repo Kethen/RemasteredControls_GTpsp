97082f5e3c09a9fdabdaa88c22050d6b66a0ed7a
Katharine Chui <kwchuiaa@connect.ust.hk>
2024-05-03 23:36:37 +0800

    hooking macro cleanup

--- hooking.h
+++ hooking.h
@@ -20,19 +20,20 @@ u32 offset_populate_car_analog_control = 0;
 { \
 	LOG("hijacking function at 0x%lx with 0x%lx", (u32)a, (u32)f); \
 	u32 _func_ = (u32)a; \
-	u32 ff = (u32)f; \
+	u32 _ff = (u32)f; \
 	int _interrupts = pspSdkDisableInterrupts(); \
+	sceKernelDcacheWritebackInvalidateAll(); \
 	if(!is_emulator){ \
-		ff = MakeSyscallStub(f); \
+		_ff = MakeSyscallStub(f); \
 	} \
 	static u32 patch_buffer[3]; \
 	_sw(_lw(_func_), (u32)patch_buffer); \
 	_sw(_lw(_func_ + 4), (u32)patch_buffer + 8);\
 	MAKE_JUMP((u32)patch_buffer + 4, _func_ + 8); \
-	_sw(0x08000000 | (((u32)(ff) >> 2) & 0x03FFFFFF), _func_); \
+	_sw(0x08000000 | (((u32)(_ff) >> 2) & 0x03FFFFFF), _func_); \
 	_sw(0, _func_ + 4); \
 	ptr = (void *)patch_buffer; \
-	sceKernelDcacheWritebackAll(); \
+	sceKernelDcacheWritebackInvalidateAll(); \
 	sceKernelIcacheClearAll(); \
 	pspSdkEnableInterrupts(_interrupts); \
 	LOG("original instructions: 0x%lx 0x%lx", _lw((u32)patch_buffer), _lw((u32)patch_buffer + 8)); \
@@ -52,68 +53,90 @@ u32 offset_populate_car_analog_control = 0;
 { \
 	LOG("hijacking syscall stub at 0x%lx with 0x%lx", (u32)a, (u32)f); \
 	u32 _func_ = (u32)a; \
-	u32 ff = (u32)f; \
+	u32 _ff = (u32)f; \
 	int _interrupts = pspSdkDisableInterrupts(); \
+	sceKernelDcacheWritebackInvalidateAll(); \
 	if(!is_emulator){ \
 		_func_ = GET_JUMP_TARGET(_lw(_func_)); \
 	} \
-	u32 pattern[2]; \
-	_sw(_lw(_func_), (u32)pattern); \
-	_sw(_lw(_func_ + 4), (u32)pattern + 4); \
+	u32 _pattern[2]; \
+	_sw(_lw(_func_), (u32)_pattern); \
+	_sw(_lw(_func_ + 4), (u32)_pattern + 4); \
 	static u32 patch_buffer[3]; \
 	if(is_emulator){ \
-		_sw(_lw((u32)pattern), (u32)patch_buffer); \
-		_sw(_lw((u32)pattern + 4), (u32)patch_buffer + 4); \
+		_sw(_lw(_func_), (u32)patch_buffer); \
+		_sw(_lw(_func_ + 4), (u32)patch_buffer + 4); \
 	}else{ \
-		_sw(_lw((u32)pattern), (u32)patch_buffer); \
-		_sw(_lw((u32)pattern + 4), (u32)patch_buffer + 8); \
+		_sw(_lw(_func_), (u32)patch_buffer); \
+		_sw(_lw(_func_ + 4), (u32)patch_buffer + 8); \
 		MAKE_JUMP((u32)patch_buffer + 4, _func_ + 8); \
 	} \
-	_sw(0x08000000 | (((u32)(ff) >> 2) & 0x03FFFFFF), _func_); \
+	_sw(0x08000000 | (((u32)(_ff) >> 2) & 0x03FFFFFF), _func_); \
 	_sw(0, _func_ + 4); \
 	ptr = (void *)patch_buffer; \
-	sceKernelDcacheWritebackAll(); \
+	sceKernelDcacheWritebackInvalidateAll(); \
 	sceKernelIcacheClearAll(); \
 	pspSdkEnableInterrupts(_interrupts); \
 	if(!is_emulator){ \
 		LOG("real hardware mode, retargetting function 0x%lx", _func_); \
 	} \
-	LOG("original instructions: 0x%lx 0x%lx", _lw((u32)pattern), _lw((u32)pattern + 4)); \
+	LOG("original instructions: 0x%lx 0x%lx", _lw((u32)_pattern), _lw((u32)_pattern + 4)); \
 	if(is_emulator){ \
-		SceUID modules[32]; \
-		SceKernelModuleInfo info; \
-		int i, count = 0; \
-		if (sceKernelGetModuleIdList(modules, sizeof(modules), &count) >= 0) { \
-			for (i = 0; i < count; i++) { \
-				info.size = sizeof(SceKernelModuleInfo); \
-				if (sceKernelQueryModuleInfo(modules[i], &info) < 0) { \
+		SceUID _modules[32]; \
+		static u32 _scan_cache[32] = {0}; \
+		SceKernelModuleInfo _info; \
+		int _i, _count = 0; \
+		if (sceKernelGetModuleIdList(_modules, sizeof(_modules), &_count) >= 0) { \
+			for (_i = 0; _i < _count; _i++) { \
+				if(_scan_cache[_i] != 0){ \
 					continue; \
 				} \
-				if (strcmp(info.name, MODULE_NAME) == 0) { \
+				_info.size = sizeof(SceKernelModuleInfo); \
+				if (sceKernelQueryModuleInfo(_modules[_i], &_info) < 0) { \
 					continue; \
 				} \
-				LOG("scanning module %s in ppsspp mode", info.name); \
-				LOG("info.text_addr: 0x%x info.text_size: 0x%x info.nsegment: 0x%x", info.text_addr, info.text_size, (int)info.nsegment); \
-				u32 j; \
-				for(j = 0;j < info.nsegment; j++){ \
-					LOG("info.segmentaddr[%ld]: 0x%x info.segmentsize[%ld]: 0x%x", j, info.segmentaddr[j], j, info.segmentsize[j]); \
+				if (strcmp(_info.name, MODULE_NAME) == 0) { \
+					continue; \
 				} \
-				if(info.text_size == 0){ \
-					if(info.nsegment >= 1 && info.segmentaddr[0] == info.text_addr){ \
-						info.text_size = info.segmentsize[0]; \
+				LOG("scanning module %s in ppsspp mode", _info.name); \
+				LOG("info.text_addr: 0x%x info.text_size: 0x%x info.nsegment: 0x%x", _info.text_addr, _info.text_size, (int)_info.nsegment); \
+				u32 _j; \
+				for(_j = 0;_j < _info.nsegment; _j++){ \
+					LOG("info.segmentaddr[%ld]: 0x%x info.segmentsize[%ld]: 0x%x", _j, _info.segmentaddr[_j], _j, _info.segmentsize[_j]); \
+				} \
+				if(_info.text_size == 0){ \
+					if(_info.nsegment >= 1 && _info.segmentaddr[0] == _info.text_addr){ \
+						_info.text_size = _info.segmentsize[0]; \
 					} \
 				} \
-				u32 k; \
-				for(k = 0; k < info.text_size; k+=4){ \
-					u32 addr = k + info.text_addr; \
-					if(/*_lw((u32)pattern) == _lw(addr + 0) &&*/ _lw((u32)pattern + 4) == _lw(addr + 4)){ \
-						LOG("found instruction pattern 0x%lx 0x%lx at 0x%lx, patching", pattern[0], pattern[1], addr); \
-						_interrupts = pspSdkDisableInterrupts(); \
-						_sw(0x08000000 | (((u32)(ff) >> 2) & 0x03FFFFFF), addr); \
-						_sw(0, addr + 4); \
-						sceKernelDcacheWritebackAll(); \
+				for(_j = 0; _j < _info.text_size; _j+=4){ \
+					u32 _addr = _j + _info.text_addr; \
+					int _found = 0; \
+					_interrupts = pspSdkDisableInterrupts(); \
+					if(_lw((u32)_pattern) == _lw(_addr + 0) && _lw((u32)_pattern + 4) == _lw(_addr + 4)){ \
+						_scan_cache[_i] = _addr; \
+						_found = 1; \
+					} \
+					pspSdkEnableInterrupts(_interrupts); \
+					if(_found){ \
+						continue; \
+					} \
+				} \
+			} \
+			for(_i = 0; _i < _count; _i++){ \
+				u32 _addr = _scan_cache[_i]; \
+				if(_addr != 0){ \
+					int _patched = 0; \
+					_interrupts = pspSdkDisableInterrupts(); \
+					if(_lw((u32)_pattern) == _lw(_addr + 0) && _lw((u32)_pattern + 4) == _lw(_addr + 4)){ \
+						_sw(0x08000000 | (((u32)(_ff) >> 2) & 0x03FFFFFF), _addr); \
+						_sw(0, _addr + 4); \
+						_patched = 1; \
 						sceKernelIcacheClearAll(); \
-						pspSdkEnableInterrupts(_interrupts); \
+					} \
+					pspSdkEnableInterrupts(_interrupts); \
+					if(_patched){ \
+						LOG("found instruction pattern 0x%lx 0x%lx at 0x%lx, patching", _pattern[0], _pattern[1], _addr); \
 					} \
 				} \
 			} \
